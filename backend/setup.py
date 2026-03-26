f3e8bf93a950254527abaf7632b5730416b64c18
UnboundSB <sidbhatt85@gmail.com>
2026-03-26 16:01:57 +0530

    FIX:  setup the enviromnent for pb11 successfully

--- backend/setup.py
+++ backend/setup.py
@@ -1,16 +1,20 @@
-from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup, Extension
+import pybind11
 
-# Define the C++ extension we want to build
 ext_modules = [
-    Pybind11Extension(
-        "hello",            # The name of the module Python will import
-        ["hello.cpp"],      # The source file
+    Extension(
+        "hello",
+        ["hello.cpp"],
+        include_dirs=[pybind11.get_include()],
+        language='c++',
+        # The compiler flags (optimization and modern C++)
+        extra_compile_args=['-std=c++17', '-O3'],
+        # The long-term fix: Force static linking of C++ standard libraries
+        extra_link_args=['-static-libgcc', '-static-libstdc++'],
     ),
 ]
 
 setup(
     name="hello",
     ext_modules=ext_modules,
-    cmdclass={"build_ext": build_ext},
 )
