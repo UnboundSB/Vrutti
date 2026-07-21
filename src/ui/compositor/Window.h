d86fcebe36a5b5edb7f9022734a1fcfa077be325
UnboundSB <sidbhatt85@gmail.com>
2026-07-21 18:03:12 +0530

    Update Window.h to accept IPCClient

--- src/ui/compositor/Window.h
+++ src/ui/compositor/Window.h
@@ -1,10 +1,11 @@
 #pragma once
 #include <string>
+#include "../../core/ipc/IPCClient.h"
 
 namespace vrutti::ui {
     class Window {
     public:
-        Window(int width, int height, const std::string& title);
+        Window(int width, int height, const std::string& title, vrutti::core::ipc::IPCClient* ipc = nullptr);
         ~Window();
 
         bool init();
@@ -17,5 +18,6 @@ namespace vrutti::ui {
         int m_height;
         std::string m_title;
         void* m_windowHandle; // Type erased webview handle
+        vrutti::core::ipc::IPCClient* m_ipc;
     };
 }
