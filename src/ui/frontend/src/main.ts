751c7440fa9ed3db3761a8cdfdd8699d532b5a25
UnboundSB <sidbhatt85@gmail.com>
2026-07-22 22:12:53 +0530

    Fix broken logo in custom title bar by using inline SVG

--- src/ui/frontend/src/main.ts
+++ src/ui/frontend/src/main.ts
@@ -97,7 +97,11 @@ export class VruttiApp extends LitElement {
     return html`
       <header>
         <div class="header-left">
-          <img src="../../../../logos/logo-16x16.png" class="logo-img" />
+          <svg class="logo-img" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 2L2 7L12 12L22 7L12 2Z" fill="#82aaff"/>
+            <path d="M2 17L12 22L22 17" stroke="#82aaff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M2 12L12 17L22 12" stroke="#82aaff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
           <div class="menu-item">File</div>
           <div class="menu-item">Edit</div>
           <div class="menu-item">Selection</div>
