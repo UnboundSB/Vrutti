7ea7433837a81b04a210fbfee66bf3c0ffe17db7
UnboundSB <sidbhatt85@gmail.com>
2026-07-25 15:16:02 +0530

    core: add initialWorkspace to Window constructor

--- src/ui/compositor/Window.h
+++ src/ui/compositor/Window.h
@@ -5,7 +5,7 @@
 namespace vrutti::ui {
     class Window {
     public:
-        Window(int width, int height, const std::string& title, vrutti::core::ipc::IPCClient* ipc = nullptr);
+        Window(int width, int height, const std::string& title, vrutti::core::ipc::IPCClient* ipc = nullptr, const std::string& initialWorkspace = "");
         ~Window();
 
         bool init();
@@ -17,6 +17,7 @@ namespace vrutti::ui {
         int m_width;
         int m_height;
         std::string m_title;
+        std::string m_initialWorkspace;
         void* m_windowHandle; // Type erased webview handle
         vrutti::core::ipc::IPCClient* m_ipc;
     };
