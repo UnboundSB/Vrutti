aa2ca484974ce851a58141ff2d76ba6016589220
UnboundSB <sidbhatt85@gmail.com>
2026-08-15 01:38:38 +0530

    chore: remove test files

--- test_settings.cpp
+++
@@ -1,3 +0,0 @@
-#include "src/core/config/SettingsManager.h"
-#include <iostream>
-int main() { std::cout << vrutti::core::config::SettingsManager::getInstance().getSettingsJson() << std::endl; return 0; }
